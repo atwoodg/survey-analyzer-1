79cdfe9580559731b57ee49eb49300d88a97a3ee
Gabriel Atwood <gabrielatwood@uvic.ca>
2025-09-27 12:04:19 -0700

    Initial commit, created while loop to read file line by line

--- survey.c
+++ survey.c
@@ -1,1 +1,13 @@
 /* survey.c */
+#include <stdio.h>
+
+int main() {
+	char buffer[300];
+
+	printf("(Taking input from file) \n");
+
+	while(fgets(buffer, sizeof(buffer), stdin) !=NULL){
+		printf("%s", buffer);
+	}
+	return 0;
+}
